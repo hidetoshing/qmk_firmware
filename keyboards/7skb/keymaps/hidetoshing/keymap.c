cbe00791e85ea34e5d7bd304389f589d1f2d11d9
hidetoshing <hidetoshing@gmail.com>
2020-09-02 14:30:06 +0900

    change keymap

--- keyboards/7skb/keymaps/hidetoshing/keymap.c
+++ keyboards/7skb/keymaps/hidetoshing/keymap.c
@@ -24,9 +24,12 @@ enum custom_keycodes {
 };
 
 #define XX_G_JA LGUI_T(KC_LANG1) // cmd or win
-#define XX_G_EN LGUI_T(KC_LANG2) // cmd or win
+#define XX_G_EN RGUI_T(KC_LANG2) // cmd or win
 #define XX_SPCR LT(_RAISE, KC_SPC) // raise or spc
 #define XX_SPCL LT(_LOWER, KC_SPC) // lower or spc
+#define XX_TG_R TG(_RAISE)
+#define XX_TG_L TG(_LOWER)
+#define XX_TG_A TG(_ADJUST)
 
 // layouts
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,28 +41,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
      KC_LCTRL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, MO(_RAISE),
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, DM_PLY1,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-               KC_LALT, XX_G_EN, XX_SPCL, XX_SPCR,              XX_SPCR, XX_SPCL,          XX_G_JA, MO(_LOWER)
+               KC_LALT, XX_G_EN, XX_SPCL, XX_SPCR,              XX_SPCR, XX_SPCL,          XX_G_JA, XX_TG_L
           //`---------------------------------------------|   |--------------------------------------------'
   ),
 
-
-  [_LOWER] = LAYOUT(
-  //,-----------------------------------------------------|   |--------------------------------------------------------------------------------.
-      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
-  //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, KC_UP  , XXXXXXX, KC_PGUP, KC_PGDN,     XXXXXXX, KC_PGUP, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-  //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
-      _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME, KC_END ,     KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_HOME, KC_END , _______,
-  //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-      _______, KC_UNDO, KC_CUT , KC_COPY,KC_PASTE, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-               _______, KC_STOP, _______, KC_ENT ,              KC_BSPC, _______,          KC_ESC , _______
-          //`---------------------------------------------|   |--------------------------------------------'
-  ),
-
-
+  // RAISE symbols
   [_RAISE] = LAYOUT(
   //,-----------------------------------------------------|   |--------------------------------------------------------------------------------.
       _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
@@ -68,15 +56,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
       _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_PIPE, _______,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-      _______, KC_MINS, KC_EQL , KC_GRV , KC_LBRC, KC_RBRC,     KC_BSLS, KC_SCLN, KC_COMM, KC_DOT , KC_SLSH, _______, _______,
+      _______, KC_MINS, KC_EQL , KC_GRV , KC_LBRC, KC_RBRC,     KC_BSLS, KC_SCLN, KC_COMM, KC_DOT , KC_SLSH, _______, KC_LOCK,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-               _______, _______, _______, _______,              _______, _______,          KC_ESC , _______
+               _______, _______, _______, _______,              _______, _______,          KC_ESC , XX_TG_R
           //`---------------------------------------------|   |--------------------------------------------'
   ),
 
+  // LOWER Move keys
+  [_LOWER] = LAYOUT(
+  //,-----------------------------------------------------|   |--------------------------------------------------------------------------------.
+      XX_TG_A, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX,       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
+  //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
+      _______, KC_WH_U, KC_MS_U, KC_WH_D, KC_BTN3, XXXXXXX,     XXXXXXX, KC_PGUP, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+  //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
+      _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1, KC_BTN2,     KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_HOME, KC_END , _______,
+  //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
+      _______, KC_UNDO, KC_CUT , KC_COPY,KC_PASTE, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DM_REC1, DM_RSTP,
+  //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
+               _______, KC_STOP, _______, KC_ENT ,              KC_BSPC, _______,          KC_ESC , XX_TG_L
+          //`---------------------------------------------|   |--------------------------------------------'
+  ),
+
+
   [_ADJUST] = LAYOUT( /* Base */
   //,-----------------------------------------------------|   |--------------------------------------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   RESET,
+      XX_TG_A, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   RESET,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
       XXXXXXX, KC_MPLY, KC_VOLU, KC_VOLD, KC_MUTE, KC_MSTP,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_VAI, RGB_VAD, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
@@ -95,20 +99,20 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   state = update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
 #ifdef RGBLIGHT_ENABLE
     switch (get_highest_layer(state)) {
-    case _LOWER:
-      rgblight_sethsv_at(HSV_RED, 0);
-      break;
     case _RAISE:
-      rgblight_sethsv_at(HSV_GREEN, 0);
+      rgblight_sethsv_at(HSV_SPRINGGREEN, 0);
+      break;
+    case _LOWER:
+      rgblight_sethsv_at(HSV_GOLDENROD, 0);
       break;
     case _ADJUST:
-      rgblight_sethsv_at(HSV_PURPLE, 0);
+      rgblight_sethsv_at(HSV_RED, 0);
       break;
     default: //  for any other layers, or the default layer
-      rgblight_sethsv_at( 0, 0, 0, 0);
+      rgblight_sethsv_at(HSV_WHITE, 0);
       break;
     }
-    rgblight_set_effect_range( 1, 5);
+    rgblight_set_effect_range( 1, RGBLED_NUM -1);
 #endif
 return state;
 }
